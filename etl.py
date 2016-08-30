e45ad17aaf68c875ba90a4c5d7590d4375971a6b
Nick Sullivan <nbsullivan@gmail.com>
2016-08-29 20:14:39 -0700

    streaming is running, change hashtag to a trending top to get tweets in real time

--- etl.py
+++ etl.py
@@ -2,6 +2,11 @@ import tweepy
 import json
 from pprint import pprint as pp
 
+class MyStreamListener(tweepy.StreamListener):
+
+    def on_status(self, status):
+        print status.text
+
 if __name__ == '__main__':
 
 	# load the API keys
@@ -21,16 +26,28 @@ if __name__ == '__main__':
 	api = tweepy.API(auth)
 
 	# search term
-	hashTag = '#tacobell'
+	hashTag = '#XFactorBR'
 
-	# get interatble cursor object.
-	cursor = tweepy.Cursor(api.search, q = hashTag).items(3000)
+	# "creating stream listener"
+	SL = MyStreamListener()
 
-	# load tweets into a list for writng to file
-	tweetlist = []
-	for item in cursor:
-		tweetlist.append(item._json)
-		print(item._json['text'])
+	# "starting stream? maybe?"
+	mystream = tweepy.Stream(auth = api.auth, listener = SL)
+
+	# "filtering?"
+	mystream.filter(track=[hashTag])
+	
+
+
+
+	# # get interatble cursor object.
+	# cursor = tweepy.Cursor(api.search, q = hashTag).items(3000)
+
+	# # load tweets into a list for writng to file
+	# tweetlist = []
+	# for item in cursor:
+	# 	tweetlist.append(item._json)
+	# 	print(item._json['text'])
 
 	# do not uncomment unless you want to override the sample tweet set.
 	# f = open('data/sampletweets.txt', 'w')
@@ -41,4 +58,4 @@ if __name__ == '__main__':
 
 	
 	# just checking number of tweets recived.
-	print len(tweetlist)
+	# print len(tweetlist)
