a46e4829ef6e1e1226a3a587e1bb405d691633c9
Nick Sullivan <nbsullivan@gmail.com>
2016-09-04 15:59:40 -0700

    streaming tweets are being saved to file, also be careful to not overwrite the files if you rerun the script. next steps, clean data and get to formated for clustering algo

--- etl.py
+++ etl.py
@@ -4,8 +4,55 @@ from pprint import pprint as pp
 
 class MyStreamListener(tweepy.StreamListener):
 
-    def on_status(self, status):
-        print status.text
+	tweetlist = []
+	tweetcounter = 0
+	batchnumber = 0
+
+	def __init__(self, api=None, searchitem = None):
+		# over ride the base _init_ method to accept a serach term parameter
+		self.api = api
+		self.searchterm = searchitem
+
+
+	def on_status(self, status):
+
+    	# we want to grab tweets in batches of 100 to send to the clustering algo, or maybe less than 100 or maybe more
+    	# also at this stage we want to have something to process the tweets.
+		self.tweetlist.append(self.clean_tweet(status._json))
+		self.tweetcounter += 1
+		print self.searchterm
+
+		print self.tweetcounter
+
+		if self.tweetcounter >= 100:
+			# once 100 tweeets have been reached we do something. for now we are just writing to a file
+			print "reached 100 tweets"
+
+			# reset the counters
+			self.tweetcounter = 0
+
+			# dump to datafile for now
+			f = open('data/{0}_batch{1}.txt'.format(self.searchterm, str(self.batchnumber)), 'w')
+			json.dump(self.tweetlist, f)
+			f.close()
+
+			# wipe the tweetlist
+			self.tweetlist = []
+
+			# increment batch couter.
+			self.batchnumber += 1
+
+        	# send the tweets to the clustering algo.
+
+	def clean_tweet(self, tweet):
+    	# do something here to clean up the tweet for processing.
+    	# this is where the preprocessing will take place.
+
+		return tweet
+
+    	
+
+
 
 if __name__ == '__main__':
 
@@ -26,10 +73,10 @@ if __name__ == '__main__':
 	api = tweepy.API(auth)
 
 	# search term
-	hashTag = '#XFactorBR'
+	hashTag = '#HowtoConfuseaMillennial'
 
 	# "creating stream listener"
-	SL = MyStreamListener()
+	SL = MyStreamListener(searchitem = hashTag)
 
 	# "starting stream? maybe?"
 	mystream = tweepy.Stream(auth = api.auth, listener = SL)
