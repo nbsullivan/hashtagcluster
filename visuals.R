e9e3dc636f569e84fdb1d78a281238c59990e2ef
Nick Sullivan <nbsullivan@gmail.com>
2016-09-25 12:11:21 -0700

    scaled counts to percentages

--- visuals.R
+++ visuals.R
@@ -16,17 +16,29 @@ for(fil in files){
   # lets only look at the top 50% words or users in a cluster
   for(k in c(min(df$Cluster):max(df$Cluster))){
     print(k)
+    print(c(min(df$Cluster):max(df$Cluster)))
+    
+    # subset the dataframe to a single cluster group
     dfa <- subset(df, Cluster == k)
-    print(quantile(dfa$Count, names = FALSE)[4])
+    
+    # rescale counts to relate to size of cluster
+    dfa$Count <- dfa$Count / sum(dfa$Count)
+    
+    # for plotting purposes only take the top 75% used words or users
     dfa <- subset(dfa, Count >= quantile(Count, names = FALSE, c(.75)))
+  
     print(head(df))
+    
+    # combind dfs back together
     new_df <- rbind(new_df,dfa)
   }
   
   df <- new_df
   
+  # set Cluster as a factor for ggplot
   df$Cluster <- as.factor(df$Cluster)
   
+  # check if word or user file
   if(grepl('user',fil)){
     print('we have a user file')
     g <- ggplot(data = df, aes(User, color = Cluster, fill = Cluster))
@@ -37,8 +49,9 @@ for(fil in files){
     g <- ggplot(data = df, aes(Word, color = Cluster, fill = Cluster))
   }
   
+  # form bar chart of counts
   g + geom_bar(aes(weight = Count)) +
-    ggtitle(fil) +
+    ggtitle(substr(fil, 1,nchar(fil)-4)) +
     theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
     facet_wrap(~Cluster, scales="free_x")
   ggsave(paste(substr(fil, 1,nchar(fil)-4),'bar.pdf'))
