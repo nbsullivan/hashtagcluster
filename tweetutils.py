e714f2f06d85428a70ae47e52c065f877dd5f96e
Nick Sullivan <nbsullivan@gmail.com>
2016-10-02 16:01:05 -0700

    tf-idf test on brangelia set

--- tweetutils.py
+++ tweetutils.py
@@ -47,71 +47,12 @@ def vectorize_tweets(tweetlist):
 
 
 def clusterinfo(n = 2, vectorized_tweets = None, names = None, tweetlistmaster = None, tweet_pred = None):
-    """we want to subset the vectorized tweets based on tweet_pred
-    also create a list of dictionaries with word counts per cluster"""
-    dict_list = []
+    """append tweet_pred to tweetmasterlist and return the dataframe"""
+    tweet_df = pd.DataFrame(tweetlistmaster)
 
-    # put everything into the full_info dict
-    full_info = {}
+    tweet_df['cluster_pred'] = tweet_pred
 
-    # extract users and tweet_ids from tweetmasterlist
-    userlist = [tweet["screen_name"] for tweet in tweetlistmaster]
-    tweet_id = [tweet['tweet_id'] for tweet in tweetlistmaster]
-    tweet_text = [tweet['text'] for tweet in tweetlistmaster]
-
-    # switch to np array for tweet texts
-    nptweets = np.array(tweet_text)
-
-
-    # loop over number of clusters
-    for k in range(n):
-        # prepare a dictionary for cluster information
-        cluster_dict = {}
-
-        # rows with label k.
-        indexes = [i for i, x in enumerate(tweet_pred) if x == k]
-
-        # extract tweet_texts for this cluster
-        subset_tweets = nptweets[indexes]
-
-        # join together tweets from list
-        tweet_string = ' '.join(subset_tweets).split()
-
-        # get counts of words
-        word_count = [tweet_string.count(i) for i in tweet_string]
-
-        # make into dict
-        word_count_dict = dict(zip(tweet_string, word_count))
-
-    
-        # get unigue users and number of tweets from them.
-        cluster_users = [userlist[i] for i in indexes]
-        user_dict = dict(Counter(cluster_users))
-
-        # get tweet ids users and number of tweets from them.
-        tweet_id_list = [tweet_id[i] for i in indexes]
-
-        # get together list of all tweets in the cluster including: well all of it.
-        cluster_tweet_list = [tweetlistmaster[i] for i in indexes]
-
-        # for idx, item in enumerate(cluster_tweet_list):
-        #     item['vector_representation'] = subset_words[idx]
-
-
-        # store everything.
-        cluster_dict["tweet_ids"] = tweet_id_list
-        cluster_dict["tweetsize"] = len(tweet_id_list)
-        cluster_dict["userscounts"] = user_dict
-        cluster_dict["bagofwords"] = word_count_dict
-        cluster_dict['tweet_data'] = cluster_tweet_list
-        # append to list
-        dict_list.append(cluster_dict)
-
-    full_info["Clusterlist"] = dict_list
-    full_info["totaltweet"] = len(tweetlistmaster)
-
-
-    return full_info
+    return tweet_df
 
 
 # def clean_tweet(tweet):
@@ -169,7 +110,7 @@ def silhouette_analysis(vectorized_tweets):
     for n in range(2,20):
         print 'testing ', n, ' clusters'
         # cluster
-        clf = MiniBatchKMeans(n_clusters=n)
+        clf = MiniBatchKMeans(n_clusters=n, )
         tweet_pred = clf.fit_predict(vectorized_tweets)
         # cluster silhouette scores
         silhouette_avg = silhouette_score(vectorized_tweets, tweet_pred)
@@ -192,51 +133,50 @@ def silhouette_analysis(vectorized_tweets):
 
     return sil_n, sil_pred_prev
 
-def counts_to_file(cluster_json, base, batchnumber):
+def counts_to_file(cluster_df, base, batchnumber, n):
     """for a cluster_json write it to a csv file, for analysis"""
 
-    # get lists of words and users
+    # initializing lists to store dictionary rows
     word_list = []
     user_list = []
 
-    # loop over the clusters
-    for j in range(len(cluster_json['Clusterlist'])):
 
-        # each instance of word or user is saved with a count and the cluster that it is part of
-        word_row = {}
-        user_row = {}
+    for k in range(0,n):
+        
+        # subset dataframe to a single cluster.
+        subset_df = cluster_df[cluster_df['cluster_pred'] == k]
 
-        # set up a word row
-        for key in cluster_json['Clusterlist'][j]['bagofwords']:
+        print 'size of cluster ', k, ':', len(subset_df.index)
 
-            word_row["Cluster"] = j
-            word_row["Word"] = key
-            word_row["Count"] = cluster_json['Clusterlist'][j]['bagofwords'][key]
+        # bag of words representation of tweets from cluster
+        word_count_dict = dict(Counter(" ".join(subset_df["text"]).split()))
 
-            word_list.append(word_row)
+        for key in word_count_dict:
+            row = {}
+            row['Word'] = key
+            row['Count'] = word_count_dict[key]
+            row['Cluster'] = k
+            word_list.append(row)
 
-            word_row = {}
+        # user count for cluster
+        user_count_dict = dict(Counter(" ".join(subset_df["screen_name"]).split()))
 
-        for key in cluster_json['Clusterlist'][j]['userscounts']:
+        for key in user_count_dict:
+            row = {}
+            row['User'] = key
+            row['Count'] = user_count_dict[key]
+            row['Cluster'] = k
+            user_list.append(row)
 
-            user_row["Cluster"] = j
-            user_row["User"] = key
-            user_row["Count"] = cluster_json['Clusterlist'][j]['userscounts'][key]
-
-            user_list.append(user_row)
-
-            user_row = {}
-
-    # put into  df for easy writing.
+    # put into df for easy writing
     user_df = pd.DataFrame(user_list)
     words_df = pd.DataFrame(word_list)
+
 
     # write to file
     user_df.to_csv(base + '{0}usercount.csv'.format(batchnumber), encoding='utf-8')
     words_df.to_csv(base + '{0}wordcount.csv'.format(batchnumber), encoding='utf-8')
 
-    print user_df.head()
-    print words_df.head()
 
 
 def tf_idf_tweets(tweetlist):
--- visuals.R
+++ visuals.R
@@ -1,6 +1,6 @@
 
 # change working directory to your data file path.
-setwd('/Users/nick/Documents/math503/project1/hashtagcluster/data/millennial')
+setwd('/Users/nick/Documents/math503/project1/hashtagcluster/data/brangelia')
 library(ggplot2)
 library(wordcloud2)
 
@@ -9,52 +9,53 @@ files = list.files(pattern="*.csv")
 
 for(fil in files){
   # load data
-  df <- read.csv(fil)
+  if(grepl('count',fil)){
+    df <- read.csv(fil)
+    
+    new_df = df[FALSE,]
+    
+    # lets only look at the top 50% words or users in a cluster
+    for(k in c(min(df$Cluster):max(df$Cluster))){
+      print(k)
+      print(c(min(df$Cluster):max(df$Cluster)))
+      
+      # subset the dataframe to a single cluster group
+      dfa <- subset(df, Cluster == k)
+      
+      # rescale counts to relate to size of cluster
+      dfa$Count <- dfa$Count / sum(dfa$Count)
+      
+      # for plotting purposes only take the top 75% used words or users
+      dfa <- subset(dfa, Count >= quantile(Count, names = FALSE, c(.75)))
+    
+      print(head(df))
+      
+      # combind dfs back together
+      new_df <- rbind(new_df,dfa)
+    }
+    
+    df <- new_df
+    
+    # set Cluster as a factor for ggplot
+    df$Cluster <- as.factor(df$Cluster)
+    
+    # check if word or user file
+    if(grepl('user',fil)){
+      print('we have a user file')
+      g <- ggplot(data = df, aes(User, color = Cluster, fill = Cluster))
   
-  new_df = df[FALSE,]
-  
-  # lets only look at the top 50% words or users in a cluster
-  for(k in c(min(df$Cluster):max(df$Cluster))){
-    print(k)
-    print(c(min(df$Cluster):max(df$Cluster)))
+      
+    } else if(grepl('word', fil)){
+      print('we have a word file')
+      g <- ggplot(data = df, aes(Word, color = Cluster, fill = Cluster))
+    }
     
-    # subset the dataframe to a single cluster group
-    dfa <- subset(df, Cluster == k)
-    
-    # rescale counts to relate to size of cluster
-    dfa$Count <- dfa$Count / sum(dfa$Count)
-    
-    # for plotting purposes only take the top 75% used words or users
-    dfa <- subset(dfa, Count >= quantile(Count, names = FALSE, c(.75)))
-  
-    print(head(df))
-    
-    # combind dfs back together
-    new_df <- rbind(new_df,dfa)
+    # form bar chart of counts
+    g + geom_bar(aes(weight = Count)) +
+      ggtitle(substr(fil, 1,nchar(fil)-4)) +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+      facet_wrap(~Cluster, scales="free_x")
+    ggsave(paste(substr(fil, 1,nchar(fil)-4),'bar.pdf'))
   }
-  
-  df <- new_df
-  
-  # set Cluster as a factor for ggplot
-  df$Cluster <- as.factor(df$Cluster)
-  
-  # check if word or user file
-  if(grepl('user',fil)){
-    print('we have a user file')
-    g <- ggplot(data = df, aes(User, color = Cluster, fill = Cluster))
-
-    
-  } else if(grepl('word', fil)){
-    print('we have a word file')
-    g <- ggplot(data = df, aes(Word, color = Cluster, fill = Cluster))
-  }
-  
-  # form bar chart of counts
-  g + geom_bar(aes(weight = Count)) +
-    ggtitle(substr(fil, 1,nchar(fil)-4)) +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-    facet_wrap(~Cluster, scales="free_x")
-  ggsave(paste(substr(fil, 1,nchar(fil)-4),'bar.pdf'))
-  
 }
 
