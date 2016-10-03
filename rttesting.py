27091353aabb4c7f461bf0712a5f128dfa6af470
MPSalkin <mpsalkin@csu.fullerton.com>
2016-10-02 18:18:16 -0700

    Adding functions to separate retweets and tweets from tweetlistmaster

---
+++ rttesting.py
@@ -0,0 +1,117 @@
+
+import glob
+import json
+from pprint import pprint as pp
+from tweetutils import clean_tweet
+from tweetutils import vectorize_tweets
+from tweetutils import clusterinfo
+from tweetutils import counts_to_file
+from tweetutils import silhouette_analysis
+from sklearn.cluster import KMeans
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
+import scipy
+from collections import Counter
+import pandas as pd
+from pprint import pprint
+from collections import defaultdict
+import itertools
+
+import operator
+
+from operator import itemgetter
+
+
+def RT_removal(tweetlistmaster):
+	#
+	RT_filter = []
+	for n in tweetlistmaster:
+
+		if n['text'][0:3] != 'RT ':
+			RT_filter.append(n)
+
+	return RT_filter
+
+
+def RT_condensor(tweetlistmaster):
+	#
+	RT_filter = []
+	for n in tweetlistmaster:
+
+		if n['text'][0:3] == 'RT ':
+			RT_filter.append(n)
+
+		dd = dict((tweet["text"], tweet) for tweet in RT_filter).values()
+
+
+	return dd
+
+if __name__ == '__main__':
+	# testing play ground, is set up to work on the #howtoconfuseamillennial dataset.
+	# make sure this reflects what is in etl.py's on_status method.
+
+	# setting up list for tweets to go into this acts like tweetlistmaster in etl
+	tweetlistmaster = []
+
+
+	# this will grab the file name where data is stored
+	for k in range(0,24):
+
+		base = 'data/millennial/HowtoConfuseaMillennial_batch'
+
+		fil = base + '{0}.txt'.format(k)
+
+		# only use millennial data
+		if "HowtoConfuseaMillennial" in fil:
+			print fil
+
+			# open and load the file
+			f = open(fil, 'r')
+			tweet_batch = json.load(f)
+
+			# append file data to full list
+			tweetlistmaster = tweetlistmaster + tweet_batch
+			userlist = [tweet["screen_name"] for tweet in tweetlistmaster]
+			tweet_id = [tweet['tweet_id'] for tweet in tweetlistmaster]
+			tweet_text = [tweet["text"] for tweet in tweetlistmaster]
+			tweet_freq = [tweet_text.count(i) for i in tweet_text]
+			tweet_count = dict(zip(tweet_text, tweet_freq))
+
+			tweet_tweet = RT_removal(tweetlistmaster)
+			retweet_list = RT_condensor(tweetlistmaster)
+
+			#pprint(tweet_tweet)
+			pprint(retweet_list)
+
+			print len(retweet_list) 
+
+			# # vectorize
+			# vectorized_tweets, names = vectorize_tweets(tweetlistmaster)
+
+			# # do silhouette analysis on master list
+			# n, tweet_pred = silhouette_analysis(vectorized_tweets)
+
+			# # create cluter information on master list
+			# cluster_json = clusterinfo(n = n, vectorized_tweets = vectorized_tweets, names = names, tweetlistmaster = tweetlistmaster, tweet_pred = tweet_pred)
+
+			# # write to file
+			# counts_to_file(cluster_json, base, batchnumber = k)
+
+			# f = open(base + '{0}.json'.format(k), 'w')
+			# json.dump(cluster_json, f)
+			# f.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
