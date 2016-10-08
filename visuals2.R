f4e2baf909b7888c4cb232410ecaabad153cbf51
Nick Sullivan <nbsullivan@gmail.com>
2016-10-07 21:56:19 -0700

    word clouds are working... there is a way to get this to run live but for right now we have a single cluster batch of WCs

--- visuals2.R
+++ visuals2.R
@@ -1,6 +1,7 @@
 library(ggplot2)
 library(wordcloud2)
 library(Hmisc)
+library(plyr)
 
 
 # change working directory to your data file path.
@@ -17,8 +18,6 @@ ggplot(data = cluster_reduced, aes(x = batch, y = size)) +
   ggtitle("Min and Max size of clusters on reduced dataset") +
   stat_summary(fun.y = "min", geom = "line", color = "green") +
   stat_summary(fun.y = "max", geom = "line", color = "purple")
-
-# 
 
 batch_info <- read.csv("#Brangelina_batchinfo.csv")
 
@@ -54,3 +53,47 @@ ggplot(data = fullbatch, aes(Clusters)) +
 ggsave("ClusterBarsplot.pdf")
 
 
+# word cloud demostration on #emmy batch 150
+
+emmy_wc <- read.csv("emmybatchpreds/#Emmys_batch150wordcount.csv")
+
+new_df = emmy_wc[FALSE,]
+
+# lets only look at the top 75% words or users in a cluster
+for(k in c(min(emmy_wc$Cluster):max(emmy_wc$Cluster))){
+  print(k)
+  print(c(min(emmy_wc$Cluster):max(emmy_wc$Cluster)))
+  
+  # subset the dataframe to a single cluster group
+  dfa <- subset(emmy_wc, Cluster == k)
+
+  # for plotting purposes only take the top 75% used words or users
+  dfa <- subset(dfa, Count >= quantile(Count, names = FALSE, c(.75)))
+
+  # rename headers for wordcloud2
+  dfa <- rename(dfa, c('Count'='freq', 'Word'='word'))
+  
+  # remove extras
+  dfa$X <-  NULL
+  
+  # wordcloud2 is a bit of a punk and needs proper order of column.
+  dfa <- dfa[c(3,2,1)]
+  
+  print(head(dfa))
+  
+  # combind dfs back together
+  new_df <- rbind(new_df,dfa)
+}
+
+emmy_wc <- new_df
+
+# wordcloud2 cannot facet.
+emmy_0 <- subset(emmy_wc, Cluster == 0)
+emmy_1 <- subset(emmy_wc, Cluster == 1)
+emmy_2 <- subset(emmy_wc, Cluster == 2)
+emmy_3 <- subset(emmy_wc, Cluster == 3)
+
+wordcloud2(emmy_0)
+wordcloud2(emmy_1)
+wordcloud2(emmy_2)
+wordcloud2(emmy_3)
